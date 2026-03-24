b77d7d84b3bde495319fe889d3004f88c9f73a08
Andrea <andreacroce46@gmail.com>
2026-03-24 16:55:04 -0400

    public final

--- src/edu/monmouth/CS205/libraryHomework3/LibraryItemConstants.java
+++ src/edu/monmouth/CS205/libraryHomework3/LibraryItemConstants.java
@@ -3,15 +3,15 @@ package edu.monmouth.CS205.libraryHomework3;
 public class LibraryItemConstants {
 	private LibraryItemConstants() {}
 	
-	protected static final int MINJOURNALVOLUME=1;
-	protected static final int MINJOURNALTITLELENGTH=1;
+	public static final int MINJOURNALVOLUME=1;
+	public static final int MINJOURNALTITLELENGTH=1;
 	
-	protected static final int MINBOOKAUTHORLENGTH=1;
-	protected static final int MINBOOKPAGES=1;
-	protected static final int MINBOOKTITLELENGTH=1;
+	public static final int MINBOOKAUTHORLENGTH=1;
+	public static final int MINBOOKPAGES=1;
+	public static final int MINBOOKTITLELENGTH=1;
 	
-	protected static final String DELIMITER = "[,.]";
-	protected static final int REQUIREDNUM = 5;
-	protected static final String BOOKITEM = "b"; 
-	protected static final String JOURNALITEM = "j"; 
+	public static final String DELIMITER = "[,.]";
+	public static final int REQUIREDNUM = 5;
+	public static final String BOOKITEM = "b"; 
+	public static final String JOURNALITEM = "j"; 
 }
